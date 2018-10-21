77ecae7800b80efce9d8a869c8f95e1b5c1c0063
AgileIntelligence <io.agileintelligence@gmail.com>
2018-10-21 10:29:55 -0400

    redux store, types, reducers

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import Navbar from "./components/Navbar";
 import ProjectBoard from "./components/ProjectBoard";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProjectTask from "./components/ProjectTask/AddProjectTask";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Navbar />
-
-          <Route exact path="/" component={ProjectBoard} />
-          <Route exact path="/addProjectTask" component={AddProjectTask} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={ProjectBoard} />
+            <Route exact path="/addProjectTask" component={AddProjectTask} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
